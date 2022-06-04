44eae4b344b7a5f4451fc351ae7ed26606286403
Milo Watanabe <milo.wata@gmail.com>
2022-06-04 13:21:47 -0400

    add some logging

--- contracts/HILOToken.sol
+++ contracts/HILOToken.sol
@@ -12,13 +12,13 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
     uint public constant HI = 0;
     uint public constant LO = 1;
 
-    // track how many transactions per price for each token
-    uint private salesPerStep = 2;
-    mapping(uint => uint) private stepCounts;
-
     // the up-to-date prices
     uint private hiPrice;
     uint private loPrice;
+
+    // track how many transactions per price for each token
+    uint private salesPerStep = 2;
+    mapping(uint => uint) private stepCounts;
 
     struct Action {
         address player;
@@ -36,13 +36,13 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
     event loIncreased(uint previousLoPrice);
     event pricesConverged(address[] parties, uint price);
 
-    constructor() ERC1155("") {
+    constructor(uint initialHi, uint initialLo) ERC1155("") {
         _mint(msg.sender, HI, 10000, "");
         _mint(msg.sender, LO, 110000, ""); // 10k for supply, 100k for HI sale rewards
 
         // set the prices
-        hiPrice = 10000;
-        loPrice = 1;
+        hiPrice = initialHi;
+        loPrice = initialLo;
 
         // set up the counts for each sale at each level: increase at 2 so it's either 0 or 1
         stepCounts[0] = 0;
@@ -67,6 +67,16 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
         }
     }
 
+    function addAction (address player, uint tokenId) private {
+        uint price = getPrice(tokenId);
+        Action memory action = Action(player, tokenId, price);
+        actions[price].push(action);
+        console.log("New action!");
+        console.log("\tplayer:", action.player);
+        console.log("\ttokenId:", action.tokenId);
+        console.log("\tprice:", action.price);
+    }
+
     function priceConverged(uint price) private {
         // prices are converged
         // get the parties
@@ -77,6 +87,12 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
 
         // game over
         _pause();
+
+        console.log("Game over! The prices converged at:", price);
+        console.log("The winners are:");
+        for (uint i = 0; i < winners.length; i++) {
+            console.log(winners[i]);
+        }
     }
 
     function getWinners() private returns (address[] storage) {
@@ -91,11 +107,13 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
     function decreaseHiPrice() private {
         hiPrice = hiPrice - 1;
         emit hiDecreased(hiPrice);
+        console.log("HI price decreased to:", hiPrice);
     }
 
     function increaseLoPrice() private {
         loPrice = loPrice + 1;
         emit loIncreased(loPrice);
+        console.log("LO price increased to:", loPrice);
     }
 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal whenNotPaused override {
@@ -110,14 +128,17 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
 
         // enforce the cost
         uint tokenId = ids[0];
-        if (tokenId == HI) require(msg.value >= hiPrice, "HI: insufficient funds");
-        if (tokenId == LO) require(msg.value >= loPrice, "LO: insufficient funds");
+        if (tokenId == HI) {
+            require(msg.value >= hiPrice, "HI: insufficient funds");
+        }
+        if (tokenId == LO) {
+            require(msg.value >= loPrice, "LO: insufficient funds");
+        }
 
         // end game if the prices converged
-        if (hiPrice == loPrice) {
+        if (to == address(0) && hiPrice == loPrice) {
             // but add the action first
-            Action memory act = Action({player: from, tokenId: tokenId, price: getPrice(tokenId)});
-            actions[act.price].push(act);
+            addAction(from, tokenId);
 
             priceConverged(getPrice(tokenId));
         }
@@ -132,8 +153,7 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
             uint count = stepCounts[tokenId];
 
             // add the action
-            Action memory act = Action({player: from, tokenId: tokenId, price: getPrice(tokenId)});
-            actions[act.price].push(act);
+            addAction(from, tokenId);
             
             // update the step count
             count = (count + 1) % salesPerStep;
@@ -148,6 +168,7 @@ contract HILOToken is ERC1155, ERC2981, Ownable, Pausable {
             // if we need to transfer LO from a HI sale, do that
             if (tokenId == HI) {
                 safeTransferFrom(address(0), from, LO, 1, data);
+                console.log("LO transferred from HI sale, recepient:", from);
             }
         }
 
